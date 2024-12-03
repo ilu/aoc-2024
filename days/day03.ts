d87bafe44198c27a0731c99b633b721893d23d42
Ilkka Järstä <jarsta@gmail.com>
2024-12-03 15:55:52 +0200

    Clean up day 3

--- days/day03.ts
+++ days/day03.ts
@@ -1,18 +1,18 @@
 export function partOne(input: string) {
-  return Array.from(input.matchAll(/mul\((\d+\,\d+)\)/gm))
+  return Array.from(input.matchAll(/mul\((\d{1,3},\d{1,3})\)/gm))
     .map((match) => match[1].split(",").map(Number))
     .reduce((acc, [a, b]) => acc + a * b, 0);
 }
 
 export function partTwo(input: string) {
   let enabled = true;
-  return Array.from(input.matchAll(/(?:mul\((\d+,\d+)\)|(don't|do))/gm))
+  return Array.from(input.matchAll(/(?:mul\((\d{1,3},\d{1,3})\)|(don't|do))/gm))
     .filter((match) => {
-      if (match[0] === "don't") {
+      if (match[2] === "don't") {
         enabled = false;
         return false;
       }
-      if (match[0] === "do") {
+      if (match[2] === "do") {
         enabled = true;
         return false;
       }
